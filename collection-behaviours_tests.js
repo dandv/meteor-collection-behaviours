2292fd24097967b3f4cc1892e1f4cc52deeb15eb
Jean-Baptiste ROUSSEAU <jeanbaptiste1981_75@hotmail.fr>
2014-09-18 09:42:18 +0000

    added some behaviours and compatibility with Meteor.users

--- collection-behaviours_tests.js
+++ collection-behaviours_tests.js
@@ -3,9 +3,7 @@ Tinytest.add("works", function(test) {
     Books.timestampable();
     Apple = new Meteor.Collection('apple' + test.id);
     Apple.timestampable();
-    if (Meteor.users) {
-        Meteor.users.timestampable();
-    }
+
     var book1 = Books.insert({
         authorId: 'author1',
         name: 'On the Origin of Species'
@@ -14,10 +12,15 @@ Tinytest.add("works", function(test) {
         authorId: 'apple1',
         name: 'On the Origin of Species'
     });
-    
-     var book = Books.findOne(book1);
-     var apple = Apple.findOne(apple1);
-     console.log(Meteor.users);
-     test.isTrue(book.hasOwnProperty('createdAt'));
-     test.isTrue(apple.hasOwnProperty('createdAt'));
+
+    var book = Books.findOne(book1);
+    var apple = Apple.findOne(apple1);
+    console.log(Meteor.users);
+    test.isTrue(book.hasOwnProperty('createdAt'));
+    test.isTrue(apple.hasOwnProperty('createdAt'));
+});
+Tinytest.addAsync("insert - Meteor.users collection document should have extra property added before being inserted and properly provide inserted _id in after hook", function(test, next) {
+    var collection = Meteor.users;
+    console.log(collection);
+    //Meteor.users.timestampable();
 });
